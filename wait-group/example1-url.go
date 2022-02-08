1b11fd3e1fa4323d9f9798df65c40a091b1828a9
Yufei Hu <yufeihu@g.ucla.edu>
2022-02-08 08:52:59 -0800

    Create example1-url.go

---
+++ wait-group/example1-url.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+type resultStruct struct {
+	url        string
+	readLength int
+	err        error
+}
+
+func getPage(url string) (int, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(body), nil
+}
+
+func main() {
+	urls := []string{
+		"http://www.google.com/",
+		"http://www.yahoo.com",
+		"http://www.bing.com",
+		"http://bbc.co.uk",
+	}
+
+	resultChan := make(chan resultStruct)
+	var wg sync.WaitGroup
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			readLength, err := getPage(url)
+			resultChan <- resultStruct{url: url, readLength: readLength, err: err}
+		}(url)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	for result := range resultChan {
+		if result.err != nil {
+			fmt.Printf("failed to fetch %q: %v\n", result.url, result.err)
+			continue
+		}
+		fmt.Printf("%s has length %d\n", result.url, result.readLength)
+	}
+}
